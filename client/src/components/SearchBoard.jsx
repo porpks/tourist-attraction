4a27351274d4cbe57cd2e9a9918e89d9a36c0076
PoR <por.pkp1997@gmail.com>
2023-08-04 14:25:11 +0700

    feat: click to show photo

--- client/src/components/SearchBoard.jsx
+++ client/src/components/SearchBoard.jsx
@@ -6,6 +6,9 @@ function SearchBoard() {
     const [message, setMessage] = useState("")
     const [isError, setIsError] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
+    const [isCpoy, setIsCopy] = useState(false)
+    const [showPhoto, setShowPhoto] = useState(false)
+    const [photo, setPhoto] = useState("")
 
     const getData = async (message) => {
         try {
@@ -31,6 +34,15 @@ function SearchBoard() {
     }
     const handleCopy = (text) => {
         navigator.clipboard.writeText(text)
+        setIsCopy(true)
+        setTimeout(() => {
+            setIsCopy(false)
+        }, 1000)
+    }
+
+    const showImage = (photo) => {
+        setShowPhoto(true)
+        setPhoto(photo)
     }
 
     return (
@@ -53,26 +65,26 @@ function SearchBoard() {
                     let length = item.tags.length
                     return (
                         <div key={item.eid} className="flex mb-12 relative">
-                            <div className="BIG-IMAGE w-1/3 max-h-[450px] overflow-hidden flex items-center rounded-3xl">
-                                <img src={item.photos[0]} className="rounded-3xl" />
+                            <div className="BIG-IMAGE w-1/3 max-h-[450px] overflow-hidden flex items-center rounded-3xl hover:scale-105">
+                                <img src={item.photos[0]} className="rounded-3xl cursor-pointer" onClick={() => showImage(item.photos[0])} />
                             </div>
                             <div className="ml-8">
                                 <a href={item.url} target="_blank">
-                                    <h1 className="TITLE text-2xl font-semibold">{item.title}</h1>
+                                    <h1 className="TITLE text-2xl font-semibold hover:underline">{item.title}</h1>
                                 </a>
                                 <p className="DESCRIPT text-lg text-slate-500 mt-2">{item.description.slice(0, 100)} ...</p>
-                                <h1 className="text-teal-500 underline"><a href={item.url} target="_blank" className="w-fit">อ่านต่อ</a></h1>
+                                <h1 className="text-teal-500 underline"><a href={item.url} target="_blank" className="w-fit hover:font-semibold">อ่านต่อ</a></h1>
                                 <h1 className="CATEGORY text-lg text-slate-600 mt-2 flex">หมวด
                                     {item.tags.map((tag, index) => {
                                         if (index !== length - 1) {
                                             return (
-                                                <h1 className="mx-2 underline cursor-pointer" onClick={() => handleCategory(tag)}>{tag}</h1>
+                                                <h1 className="mx-2 underline cursor-pointer hover:font-semibold hover:no-underline" onClick={() => handleCategory(tag)}>{tag}</h1>
                                             )
                                         } else {
                                             return (
                                                 <>
                                                     <h1 className="mx-2">และ</h1>
-                                                    <h1 className="mx-2 underline cursor-pointer" onClick={() => handleCategory(tag)}>{tag}</h1>
+                                                    <h1 className="mx-2 underline cursor-pointer hover:font-semibold hover:no-underline" onClick={() => handleCategory(tag)}>{tag}</h1>
                                                 </>
                                             )
                                         }
@@ -82,23 +94,34 @@ function SearchBoard() {
                                         if (index !== 0) {
                                             return (
                                                 <div className="w-[120px] h-[120px] mt-4 mr-8 hover:scale-110">
-                                                    <img src={image} className="h-full rounded-2xl" />
+                                                    <img src={image} className="h-full rounded-2xl cursor-pointer" onClick={() => showImage(image)} />
                                                 </div>
                                             )
                                         }
                                     })}
                                 </div>
                                 <div
-                                    className="text-teal-500 text-2xl rounded-full border-2 border-teal-500 p-1 absolute right-0 bottom-0 hover:bg-teal-500 hover:text-white hover:cursor-pointer shadow transform active:scale-75 transition-transform"
+                                    className="COPY-LINK text-teal-500 text-2xl rounded-full border-2 border-teal-500 p-1 absolute right-0 bottom-0 hover:bg-teal-500 hover:text-white hover:cursor-pointer shadow transform active:scale-75 transition-transform"
                                     onClick={() => handleCopy(item.url)}>
-                                    <i class="fa-solid fa-link"></i>
+                                    <i className="fa-solid fa-link"></i>
                                 </div>
-                                {/* <h1 className="absolute -right-10 bottom-12 bg-slate-300 rounded text-sm px-2">copy to clipboard</h1> */}
                             </div>
                         </div>
                     )
                 })}
             </div>
+            {/* <h1 className="rounded text-xl px-2 fixed bottom-6 right-10">copy to clipboard</h1> */}
+            {showPhoto ? <>
+                <div className="SHOW-PHOTO flex justify-center items-center w-full h-[100vh] fixed top-0 left-0 bg-black bg-opacity-60">
+                    <div className="max-w-[50vw] max-h-[80vh] relative">
+                        <img src={photo} className="max-w-full max-h-full" />
+                        <i className="fa-solid fa-xmark absolute text-slate-400 text-4xl -top-8 -right-8 hover:text-white hover:cursor-pointer hover:outline"
+                            onClick={() => setShowPhoto(false)}>
+                        </i>
+                    </div>
+                </div>
+            </> : null}
+
             {isError ? <h1 className="py-12 text-4xl">Request failed</h1> : null}
             {isLoading ? <h1 className="py-12 text-4xl">Loading ....</h1> : null}
         </>
